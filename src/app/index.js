323095c333085004fe18fbe13779cb2a8f449a69
juanmnl <juanmnlcornejo@gmail.com>
2018-01-05 21:51:37 -0500

    Add more logic and animations

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import '../style/app.scss';
 
-console.log('Hello Wolfpack? lol!');
+console.log('Hello, Wolfpack!');
 
 let toggleLayout = document.getElementById('toggleLayout');
 let screen = document.body;
@@ -12,22 +12,17 @@ toggleLayout.onclick = function() {
 
 // ***************************************
 var squares = document.querySelectorAll('.square');
-var h2 = document.getElementsByTagName('h2')[0];
+var body = document.getElementsByTagName('body')[0];
 var guess = document.getElementById('guess');
 var message = document.getElementById('message');
+var resetBtn = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easy');
+var hardBtn = document.querySelector('#hard');
+var scream = document.getElementsByClassName('scream')[0];
 
-var colors = [
-  'rgb(255, 0, 0)',
-  'rgb(255, 255, 0)',
-  'rgb(0, 255, 0)',
-  'rgb(0, 255, 255)',
-  'rgb(0, 0, 255)',
-  'rgb(255, 0, 255)'
-];
-
-// ***** COLORS *****
+// ***** MAIN LOGIC *****
+var colors = generateRandomColors(6);
 var pickedColor = pickColor();
-var bodyBg = document.body.style.backgroundColor;
 
 for (let i = 0; i < squares.length; i++) {
   // Add initial colors to squares
@@ -39,29 +34,80 @@ for (let i = 0; i < squares.length; i++) {
     // Compare clicked color to picked color
     if (clickedColor === pickedColor) {
       message.textContent = 'You champ!';
+      scream.textContent = 'Winner!';
       changeColors(pickedColor);
+      scream.style.color = 'white';
+      scream.textContent = 'Winner';
+      scream.classList.add('scream-now');
+      setTimeout(function() {
+        scream.classList.remove('scream-now');
+      }, 600);
+      resetBtn.textContent = 'Play again?';
     } else {
-      this.style.display = 'none';
+      this.style.backgroundColor = document.body.style.backgroundColor;
       message.textContent = 'Pffs! Lame!';
+      scream.textContent = 'Oh Nooo!';
+      scream.classList.add('scream-now');
+      setTimeout(function() {
+        scream.classList.remove('scream-now');
+        scream.textContent = '';
+      }, 600);
     }
   });
 }
 
+// ***** ACTIONS *****
+resetBtn.addEventListener('click', reset);
+easyBtn.addEventListener('click', function() {
+  hardBtn.classList.remove('selected');
+  this.classList.add('selected');
+});
+hardBtn.addEventListener('click', function() {
+  easyBtn.classList.remove('selected');
+  this.classList.add('selected');
+});
+
 // ***** COLOR TO GUESS *****
 guess.textContent = pickedColor;
+
+// ***** FUNCTIONS *****
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(x) {
+  let arr = [];
+  for (let i = 0; i < x; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 
 function changeColors(color) {
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = color;
   }
-  h2.style.backgroundColor = color;
+  body.style.backgroundColor = color;
 }
 
-function getRandomVal(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-function pickColor() {
-  var random = Math.floor(Math.random() * colors.length);
-  return colors[random];
+function reset() {
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  guess.textContent = pickedColor;
+  body.style.backgroundColor = '';
+  for (let i = 0; i < colors.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  resetBtn.textContent = 'New Colors';
+  message.textContent = '';
+  scream.textContent = '';
+  scream.style.color = '';
 }
