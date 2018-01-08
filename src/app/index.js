522f25ef6efa4cccafdcf80895285352dc527377
juanmnl <juanmnlcornejo@gmail.com>
2018-01-08 11:30:35 -0500

    Refactor modes buttons and remove layout tool

--- src/app/index.js
+++ src/app/index.js
@@ -2,23 +2,13 @@ import '../style/app.scss';
 
 console.log('Ah! Ah! Ah! You cheater!');
 
-// Layout tool
-let toggleLayout = document.getElementById('toggleLayout');
-let screen = document.body;
-
-toggleLayout.onclick = function() {
-  screen.classList.toggle('layout');
-  toggleLayout.classList.toggle('active');
-};
-
 // **********************************************************
 var squares = document.querySelectorAll('.square');
 var body = document.getElementsByTagName('body')[0];
 var guess = document.getElementById('guess');
 var message = document.getElementById('message');
 var resetBtn = document.querySelector('#reset');
-var easyBtn = document.querySelector('#easy');
-var hardBtn = document.querySelector('#hard');
+var modeBtns = document.querySelectorAll('.mode');
 var scream = document.getElementsByClassName('scream')[0];
 var scoreDisplay = document.querySelector('h1 > span');
 var gamesDisplay = document.querySelector('.board > .games');
@@ -34,14 +24,10 @@ var games = 1;
 gamesDisplay.textContent = `Game #${games}`;
 
 for (let i = 0; i < squares.length; i++) {
-  // Add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
-  // Add click listeners to squares
   squares[i].addEventListener('click', function() {
-    // Grap picked color
     var clickedColor = this.style.backgroundColor;
     count++;
-    // Compare clicked color to picked color
     if (clickedColor === pickedColor) {
       win();
       if (count === 1) {
@@ -56,29 +42,53 @@ for (let i = 0; i < squares.length; i++) {
       score -= 100;
       this.style.backgroundColor = body.style.backgroundColor;
     }
-    console.log(count);
-    console.log(score);
     scoreDisplay.textContent = score;
   });
 }
 
 // ***** UI CONTROLS *****
 resetBtn.addEventListener('click', reset);
-easyBtn.addEventListener('click', function() {
-  easyMode();
-});
-hardBtn.addEventListener('click', function() {
-  hardMode();
-});
+
+for (let i = 0; i < modeBtns.length; i++) {
+  modeBtns[i].addEventListener('click', function() {
+    modeBtns[0].classList.remove('selected');
+    modeBtns[1].classList.remove('selected');
+    this.classList.add('selected');
+    this.textContent === 'Easy' ? (numSquares = 3) : (numSquares = 6);
+    reset();
+  });
+}
 
 // ***** FUNCTIONS *****
+function reset() {
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  count = 0;
+  guess.textContent = pickedColor;
+  body.style.backgroundColor = '#f5f5f5';
+  for (let i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.display = 'block';
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = 'none';
+    }
+  }
+  resetBtn.textContent = 'New Colors';
+  message.textContent = 'Pick a color';
+  scream.textContent = '';
+  scream.style.color = '';
+  games++;
+  gamesDisplay.textContent = `Game #${games}`;
+}
+
 function win() {
   if (count === 1) {
     message.textContent = '+ 300';
     screamer('Sweet!', 'white');
   } else if (count === 6) {
     message.textContent = '+ 50';
-    screamer('Well...', 'white');
+    screamer('You know nothing!', 'white');
   } else {
     message.textContent = '+ 100';
     screamer('Winner!', 'white');
@@ -93,43 +103,6 @@ function nope() {
     screamer('You Suck!', 'black');
   } else {
     screamer('Nope', 'black');
-  }
-}
-
-function easyMode() {
-  hardBtn.classList.remove('selected');
-  easyBtn.classList.add('selected');
-  count = 0;
-  numSquares = 3;
-  colors = generateRandomColors(numSquares);
-  pickedColor = pickColor();
-  guess.textContent = pickedColor;
-  message.textContent = '';
-  scream.textContent = '';
-  body.style.backgroundColor = '#f5f5f5';
-  for (let i = 0; i < squares.length; i++) {
-    if (colors[i]) {
-      squares[i].style.backgroundColor = colors[i];
-    } else {
-      squares[i].style.display = 'none';
-    }
-  }
-}
-
-function hardMode() {
-  easyBtn.classList.remove('selected');
-  hardBtn.classList.add('selected');
-  count = 0;
-  numSquares = 6;
-  colors = generateRandomColors(numSquares);
-  pickedColor = pickColor();
-  guess.textContent = pickedColor;
-  message.textContent = '';
-  scream.textContent = '';
-  body.style.backgroundColor = '#f5f5f5';
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-    squares[i].style.display = '';
   }
 }
 
@@ -160,45 +133,15 @@ function changeColors(color) {
   body.style.backgroundColor = color;
 }
 
-function reset() {
-  colors = generateRandomColors(numSquares);
-  pickedColor = pickColor();
-  count = 0;
-  guess.textContent = pickedColor;
-  body.style.backgroundColor = '#f5f5f5';
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-  }
-  resetBtn.textContent = 'New Colors';
-  message.textContent = 'Continue...';
-  scream.textContent = '';
-  scream.style.color = '';
-  // for (let i = 0; i < squares.length; i++) {
-  //   squares[i].style.display = '';
-  // }
-  games++;
-  gamesDisplay.textContent = `Game #${games}`;
-}
-
 // Shouts a word on click (winner, loser, etc...)
 function screamer(str, color) {
   scream.style.color = color;
   scream.textContent = str;
   scream.classList.add('scream-now');
   setTimeout(function() {
-    // review this ! **************************************
-    if (str !== 'Winner!' || str !== 'Sweet!' || str !== 'Well...') {
+    if (str === 'Nope' || str === 'You Suck!') {
       scream.textContent = '';
     }
     scream.classList.remove('scream-now');
   }, 600);
-}
-
-function generateSquares(n) {
-  var arr = [];
-  var template = `<div class="square"></div>`;
-  for (let i = 0; i < n; i++) {
-    arr.push(template);
-  }
-  return arr;
 }
