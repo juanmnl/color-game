887467fb8f9add1bfa8492e793636efd49fb0889
juanmnl <juanmnlcornejo@gmail.com>
2018-01-07 23:52:41 -0500

    Add messages, refactor game modes into fns

--- src/app/index.js
+++ src/app/index.js
@@ -23,15 +23,15 @@ var scream = document.getElementsByClassName('scream')[0];
 var scoreDisplay = document.querySelector('h1 > span');
 var gamesDisplay = document.querySelector('.board > .games');
 
-// ***** MAIN LOGIC *****
+// ***** MAIN UI *****
 var numSquares = 6;
 var colors = generateRandomColors(numSquares);
 var pickedColor = pickColor();
 guess.textContent = pickedColor;
 var score = 0;
 var count = 0;
-var games = 0;
-gamesDisplay.textContent = `Games Played: ${games}`;
+var games = 1;
+gamesDisplay.textContent = `Game #${games}`;
 
 for (let i = 0; i < squares.length; i++) {
   // Add initial colors to squares
@@ -46,13 +46,15 @@ for (let i = 0; i < squares.length; i++) {
       win();
       if (count === 1) {
         score += 300;
+      } else if (count === 6) {
+        score += 50;
       } else {
         score += 100;
       }
     } else {
       nope();
       score -= 100;
-      this.style.display = 'none';
+      this.style.backgroundColor = body.style.backgroundColor;
     }
     console.log(count);
     console.log(score);
@@ -60,17 +62,51 @@ for (let i = 0; i < squares.length; i++) {
   });
 }
 
-// ***** ACTIONS *****
+// ***** UI CONTROLS *****
 resetBtn.addEventListener('click', reset);
 easyBtn.addEventListener('click', function() {
+  easyMode();
+});
+hardBtn.addEventListener('click', function() {
+  hardMode();
+});
+
+// ***** FUNCTIONS *****
+function win() {
+  if (count === 1) {
+    message.textContent = '+ 300';
+    screamer('Sweet!', 'white');
+  } else if (count === 6) {
+    message.textContent = '+ 50';
+    screamer('Well...', 'white');
+  } else {
+    message.textContent = '+ 100';
+    screamer('Winner!', 'white');
+  }
+  changeColors(pickedColor);
+  resetBtn.textContent = 'Play again?';
+}
+
+function nope() {
+  message.textContent = '-100';
+  if (count === 3) {
+    screamer('You Suck!', 'black');
+  } else {
+    screamer('Nope', 'black');
+  }
+}
+
+function easyMode() {
   hardBtn.classList.remove('selected');
-  this.classList.add('selected');
+  easyBtn.classList.add('selected');
+  count = 0;
   numSquares = 3;
   colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   guess.textContent = pickedColor;
   message.textContent = '';
   scream.textContent = '';
+  body.style.backgroundColor = '#f5f5f5';
   for (let i = 0; i < squares.length; i++) {
     if (colors[i]) {
       squares[i].style.backgroundColor = colors[i];
@@ -78,37 +114,23 @@ easyBtn.addEventListener('click', function() {
       squares[i].style.display = 'none';
     }
   }
-});
-hardBtn.addEventListener('click', function() {
+}
+
+function hardMode() {
   easyBtn.classList.remove('selected');
-  this.classList.add('selected');
+  hardBtn.classList.add('selected');
+  count = 0;
   numSquares = 6;
   colors = generateRandomColors(numSquares);
   pickedColor = pickColor();
   guess.textContent = pickedColor;
   message.textContent = '';
   scream.textContent = '';
+  body.style.backgroundColor = '#f5f5f5';
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
     squares[i].style.display = '';
   }
-});
-
-// ***** FUNCTIONS *****
-function win() {
-  if (count === 1) {
-    message.textContent = '+ 300';
-  } else {
-    message.textContent = '+ 100';
-  }
-  changeColors(pickedColor);
-  screamer('Winner!', 'white');
-  resetBtn.textContent = 'Play again?';
-}
-
-function nope() {
-  message.textContent = '-100';
-  screamer('Nope', 'black');
 }
 
 function pickColor() {
@@ -143,19 +165,19 @@ function reset() {
   pickedColor = pickColor();
   count = 0;
   guess.textContent = pickedColor;
-  body.style.backgroundColor = '';
+  body.style.backgroundColor = '#f5f5f5';
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
   resetBtn.textContent = 'New Colors';
-  message.textContent = '';
+  message.textContent = 'Continue...';
   scream.textContent = '';
   scream.style.color = '';
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].style.display = '';
-  }
+  // for (let i = 0; i < squares.length; i++) {
+  //   squares[i].style.display = '';
+  // }
   games++;
-  gamesDisplay.textContent = `Games Played: ${games}`;
+  gamesDisplay.textContent = `Game #${games}`;
 }
 
 // Shouts a word on click (winner, loser, etc...)
@@ -165,7 +187,7 @@ function screamer(str, color) {
   scream.classList.add('scream-now');
   setTimeout(function() {
     // review this ! **************************************
-    if (str !== 'Winner!') {
+    if (str !== 'Winner!' || str !== 'Sweet!' || str !== 'Well...') {
       scream.textContent = '';
     }
     scream.classList.remove('scream-now');
